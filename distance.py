ab40ac9db3f13bae2b7cb92b3881dec2979dcfac
Todd Boone II <toddbooneii@Todds-MacBook-Pro.local>
2018-11-14 00:30:18 -0500

    Initial commit

---
+++ distance.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+'''
+distance.py
+
+Enables the CanCan application to retrieve information from 
+Google's Distance Matrix API to retrieve distance information.
+
+https://developers.google.com/maps/documentation/distance-matrix/intro
+'''
