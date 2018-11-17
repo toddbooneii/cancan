5a996eb8e5a732d39b7d842f31ea13ea5d980d4a
Todd Boone II <tjboone.817@gmail.com>
2018-11-17 12:34:25 -0500

    Update header info

--- distance.py
+++ distance.py
@@ -2,8 +2,8 @@
 '''
 distance.py
 
-Enables the CanCan application to retrieve information from 
-Google's Distance Matrix API to retrieve distance information.
+Enables the CanCan application to retrieve distance 
+information from Google's Distance Matrix API.
 
 https://developers.google.com/maps/documentation/distance-matrix/intro
 '''
